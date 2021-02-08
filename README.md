22b4c36c1bb2dbb33a6cbaf10b633d7a41493438
Smaine Kahlouch <smainklh@gmail.com>
2021-02-08 08:40:24 +0100

    :recycle: changing how to enable completions

--- README.md
+++ README.md
@@ -3,32 +3,44 @@
 ## bash-it
 install `bash-it`
 
-```
+```console
 $ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ```
 
-```
+```console
 $ ~/.bash_it/install.sh -s
 ```
 
-```
+```console
 $ cat dot-bashrc >> ~/.bashrc
 ```
 
-```
+```console
 $ bashit enable plugin ssh tmux go git
 ```
 
+## Enable completions
+Put the desired completions into ~/.bash_it/completions/available
+```console
+kubectl completion bash > ~/.bash_it/completion/available/kubectl.completion.bash
+```
+
+And enable them
+```console
+bash_it enable completion ssh virsh github-cli go docker-compose docker awscli gcloud vault flux helm k3d kubectl skaffold tmux
+```
+
+
 ## awesome-vimrc
 
-```
+```console
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 ```
 
 # copy my config files
 
-```
+```console
 cp dot-bashrc ~/.bashrc
 cp bash_it-custom.bash ~/.bash_it/custom/custom.bash
 cp dot-kube.tmux ~/.kube.tmux
--- bash_it-custom.bash
+++ bash_it-custom.bash
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 # custom directory.
 
+eval "$(starship init bash)"
+
 export PROMPT_COMMAND="${PROMPT_COMMAND};history -a; history -n"
 
-if [ -n "$DESKTOP_SESSION" ];then
-    eval $(gnome-keyring-daemon --start)
-    export SSH_AUTH_SOCK
-fi
-
-export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
+#if [ -n "$DESKTOP_SESSION" ];then
+#    eval $(gnome-keyring-daemon --start)
+#    export SSH_AUTH_SOCK
+#fi
+#
+#export GPG_TTY="$(tty)"
+#export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+#gpgconf --launch gpg-agent
 
 # History Settings
 export HISTSIZE=2000000
@@ -18,31 +20,11 @@ export HISTFILESIZE=3000000
 export TIMEFORMAT=$'\nreal %3R\tuser %3U\tsys %3S\tpcpu %P\n'
 export HISTTIMEFORMAT="%d/%m/%Y - %H:%M:%S > "
 export HISTIGNORE="&:bg:fg:ll:"
-
-# Dailymotion
-PATH=${PATH}:~/sources/dailymotion/dm/bin
-export VAULT_ADDR=https://vault.dm.gg:8200
-
-if [ -f ~/.kube/kubectl_completion ]; then
-     . ~/.kube/kubectl_completion
-fi
 export KUBE_EDITOR=vim
 
-if [ -f ~/.kube/helm_completion ]; then
-     . ~/.kube/helm_completion
-fi
+# Completions are managed through bash_it
 
-if [ -f ~/.kube/kubectl_aliases ]; then
-     . ~/.kube/kubectl_aliases
-fi
-
-if [ -f ~/.kube/k3d_completion ]; then
-     . ~/.kube/k3d_completion
-fi
-
-if [ -f ~/.skaffold_completion ]; then
-     . ~/.skaffold_completion
-fi
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
 # Krew
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
@@ -51,18 +33,19 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
 
-GOPATH=~/go
+GOPATH=~/sources/go
 PATH=${PATH}:${GOPATH}/bin
+
 # Kubebuilder
 export PATH=$PATH:/usr/local/kubebuilder/bin
 
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+# Cargo (Rust)
+export PATH=$PATH:$HOME/.cargo/bin
 
-[ -f ~/.fzf.bash  ] && source ~/.fzf.bash
 
 #if [[ $TMUX ]]; then source ~/.tmux-git/tmux-git.sh; fi
 
-/usr/bin/gpg-connect-agent updatestartuptty /bye  
+# /usr/bin/gpg-connect-agent updatestartuptty /bye
 
 # My Aliases
 source ~/.bash_it/aliases/smana.bash
